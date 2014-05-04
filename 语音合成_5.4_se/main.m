f83b4089e535caa387167db9c9fcecebccc821fe
qianfeng <apple@aofel.local>
2014-05-04 17:17:59 +0800

    Initial Commit

---
+++ 语音合成_5.4_se/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  语音合成_5.4_se
+//
+//  Created by qianfeng on 14-5-4.
+//  Copyright (c) 2014年 aofel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ 语音合成_5.4_seTests/_____5_4_seTests.m
@@ -0,0 +1,34 @@
+//
+//  _____5_4_seTests.m
+//  语音合成_5.4_seTests
+//
+//  Created by qianfeng on 14-5-4.
+//  Copyright (c) 2014年 aofel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _____5_4_seTests : XCTestCase
+
+@end
+
+@implementation _____5_4_seTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
